c9d7609150769068827622a5d44bc79fb269d75e
LeSofanthiel <neilmccuistion@gmail.com>
2026-05-14 14:50:16 -0600

    Update config_github_terminal.js to actualy turn on display (whoops)

--- config_github_terminal.js
+++ config_github_terminal.js
@@ -3,9 +3,9 @@ export const diskImageUrl = IMAGE_URL;
 // The root filesystem backend type
 export const diskImageType = "github";
 // Print an introduction message about the technology
-export const printIntro = true;
+export const printIntro = false;
 // Is a graphical display needed
-export const needsDisplay = false;
+export const needsDisplay = true;
 // Executable full path (Required)
 export const cmd = CMD; // Default: "/bin/bash";
 // Arguments, as an array (Required)
